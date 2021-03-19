23eb1e9a81bbe7c9e449b39ddca9df9efb5eee9e
RuslanOsmonov <79210459+RuslanOsmonov@users.noreply.github.com>
2021-03-19 19:03:41 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const toggler = document.querySelector('.toggler');
+const navigation = document.querySelector('.navigation');
+
+toggler.addEventListener('click', () => {
+  toggler.classList.toggle('active')
+  navigation.classList.toggle('active')
+});
